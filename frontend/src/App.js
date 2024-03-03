91d81a91aa6afdc4dd6f6433fa8881ce494133a7
kaisboubakri <kais.boubakri1998@gmail.com>
2024-03-03 20:30:15 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import MainContainer from "./Components/MainContainer";
+import Login from "./Components/Login";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./Components/Welcome";
+import ChatArea from "./Components/ChatArea";
+import Users from "./Components/Users";
+import CreateGroups from "./Components/CreateGroups";
+import Groups from "./Components/Groups";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MainContainer /> */}
+      {/* <Login /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="app" element={<MainContainer />}>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="chat/:_id" element={<ChatArea />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="groups" element={<Groups />}></Route>
+          <Route path="create-groups" element={<CreateGroups />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/MainContainer.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from "react";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export const myContext = createContext();
+function MainContainer() {
+  const lightTheme = useSelector((state) => state.themeKey);
+  const [refresh, setRefresh] = useState(true);
+
+  return (
+    <div className={"main-container" + (lightTheme ? "" : " dark")}>
+      <myContext.Provider value={{ refresh: refresh, setRefresh: setRefresh }}>
+        <Sidebar />
+        <Outlet />
+      </myContext.Provider>
+      {/* <Welcome /> */}
+      {/* <CreateGroups /> */}
+      {/* <ChatArea props={conversations[0]} /> */}
+      {/* <Users /> */}
+      {/* <Groups /> */}
+    </div>
+  );
+}
+
+export default MainContainer;
